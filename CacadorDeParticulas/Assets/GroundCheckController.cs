e167b699eda9dcf2864226003de0ad2373d47105
jhonatadam <jhonatamatias@hotmail.com>
2016-10-20 02:31:56 -0300

    animacao do player completamente bugada

---
+++ CacadorDeParticulas/Assets/GroundCheckController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheckController : MonoBehaviour {
+
+	private bool grounded = false;
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Ground")
+		{
+			print ("grounded");
+			grounded = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Ground")
+		{
+			print ("ungrounded");
+			grounded = false;
+		}
+	}
+
+	public bool isGrounded ()
+	{
+		return grounded;
+
+	}
+}
---
+++ CacadorDeParticulas/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float jumpForce;
+
+	public GroundCheckController groundCheck;
+
+	private Rigidbody2D rb2d;
+	private SpriteRenderer sr;
+	private Animator anim;
+
+	private bool isFacingRight = true;
+	
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (groundCheck.isGrounded ()) {
+			if (Input.GetKeyDown (KeyCode.W)) {
+				rb2d.AddForce (new Vector2 (0, jumpForce));
+				// saltando
+				anim.SetTrigger ("jump");
+			}
+		}
+
+
+	}
+
+	void FixedUpdate() {
+		float horizontalMoviment = Input.GetAxis ("Horizontal");
+
+		UpdatePlayerVelocity (horizontalMoviment);
+		UpdateSpriteDirection (horizontalMoviment);
+		UpdateAnimator ();
+	}
+
+	private void UpdatePlayerVelocity (float horizontalMovement) {
+		rb2d.velocity = new Vector2 (horizontalMovement * speed, rb2d.velocity.y);
+	}
+
+	private void UpdateSpriteDirection (float horizontalMovement) {
+		if (horizontalMovement < 0.0f) {
+			sr.flipX = true; 
+		} else if (horizontalMovement > 0.0f) {
+			sr.flipX = false;
+		} 
+	}
+
+	private void UpdateAnimator () {
+		if (groundCheck.isGrounded ()) {
+			if (rb2d.velocity.x != 0.0f) {
+				anim.SetTrigger ("walk");
+			} else {
+				anim.SetTrigger ("stop");
+			}
+			anim.SetTrigger ("touchground");
+		} else {
+			if (rb2d.velocity.y < 0.0f) {
+				anim.SetTrigger ("fall");
+			} else if (rb2d.velocity.y < 0.2f) {
+				anim.SetTrigger ("jumpfall");
+			} else {
+
+			}
+		}
+
+		//if (anim.GetCurrentAnimatorStateInfo (0).IsName ("JumpDown")) {
+		//	anim.SetTrigger ("fall");
+		//} else 
+
+	}
+}
