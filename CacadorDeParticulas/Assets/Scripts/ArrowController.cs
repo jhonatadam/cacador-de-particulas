5188354d8e30014c6ec521d9bcaed99915c1cb63
jhonatadam <jhonatamatias@hotmail.com>
2016-11-05 12:45:00 -0300

    lab0
    alterado lab 0

---
+++ CacadorDeParticulas/Assets/Scripts/ArrowController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowController : MonoBehaviour {
+	
+	public float speed;
+	public float amplitude;
+	public string direction;
+
+	private float amplitudeSum = 0.0f;
+	private Vector3 initialPosition;
+	private SpriteRenderer sr;
+	public SpriteRenderer keySr;
+
+	void Start () {
+		initialPosition = transform.position;
+		sr = GetComponent<SpriteRenderer> ();
+	}
+
+	void FixedUpdate () {
+		if (direction == "Up") {
+			moveArrow ('Y', -1);
+		} else if (direction == "Down") {
+			moveArrow ('Y', 1);
+		} else if (direction == "Right") {
+			moveArrow ('X', 1);
+		} else if (direction == "Left"){
+			moveArrow ('X', -1);
+		}
+
+		amplitudeSum += speed;
+
+		if (amplitudeSum > amplitude) {
+			transform.position = initialPosition;
+			amplitudeSum = 0.0f;
+		}
+			
+		Color tempColor = new Color (sr.color.r, sr.color.g, sr.color.b, (1-amplitudeSum/amplitude));
+		sr.color = tempColor;
+		keySr.color = tempColor;
+	}
+
+	void moveArrow (char axis, int direct) {
+		if (axis == 'X') {
+			transform.Translate (new Vector3(direct * speed, 0, 0));
+		} else if (axis == 'Y') {
+			transform.Translate (new Vector3(0, direct * speed, 0));			
+		}
+	}
+}
--- CacadorDeParticulas/Assets/Scripts/GroundCheckController.cs
+++ CacadorDeParticulas/Assets/Scripts/GroundCheckController.cs
@@ -4,22 +4,30 @@ using System.Collections;
 public class GroundCheckController : MonoBehaviour {
 
 	private bool grounded = false;
+	private ArrayList groundTag;
+
+	void Start () {
+		groundTag = new ArrayList ();
+
+		groundTag.Add ("Ground");
+		groundTag.Add ("Elevator");
+	}
 
 	void OnTriggerStay2D(Collider2D other)
 	{
-		if (other.gameObject.tag == "Ground")
-		{
-
-			grounded = true;
+		foreach (string tag in groundTag) {
+			if (other.gameObject.tag == tag) {
+				grounded = true;
+			}
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D other)
 	{
-		if (other.gameObject.tag == "Ground")
-		{
-
-			grounded = false;
+		foreach (string tag in groundTag) {
+			if (other.gameObject.tag == tag) {
+				grounded = false;
+			}
 		}
 	}
 
