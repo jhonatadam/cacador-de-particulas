8dba272b733750815997d05e75764fb8dd84a692
jhonatadam <jhonatamatias@hotmail.com>
2016-11-16 22:02:49 -0300

    adicionado novo cenário

---
+++ CacadorDeParticulas/Assets/ElevatorController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorController : MonoBehaviour {
+
+	public Vector3 topPosition;
+	public Vector3 downPosition;
+	public Vector3 speed;
+
+	private bool isInTop = false;
+
+	private bool playerIn = false;
+
+	private bool move = false;
+
+	void Start () {
+		downPosition = transform.position;
+	}
+
+	void Update () {
+		if (playerIn) {
+			if (Input.GetKeyDown (KeyCode.F)) {
+				move = true;
+			}
+		}
+
+		if (move) {
+			if (!isInTop) {
+				if (transform.position.y < topPosition.y) {
+					transform.Translate (speed);
+				} else {
+					isInTop = true;
+					move = false;
+				}
+			} else {
+				if (transform.position.y > downPosition.y) {
+					transform.Translate (-1*speed);
+				} else {
+					isInTop = false;
+					move = false;
+				}
+			}
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			playerIn = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			playerIn = false;
+		}
+	}
+}
