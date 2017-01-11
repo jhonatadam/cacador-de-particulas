48c131ef42c831ee6bdbe6d82dbf7770f3e500a7
jhonatadam <jhonatamatias@hotmail.com>
2017-01-11 09:37:29 -0300

    Adicionado sprite elevador subindo e descendo

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -24,17 +24,38 @@ public class Elevator : MonoBehaviour {
 	public GameObject player;
 	public GameObject camera;
 
+	// referencia para o sprite do elevador
+	SpriteRenderer sr;
+
+	// sprites do elevador
+	Sprite stopSprite;
+	Sprite upSprite;
+	Sprite downSprite;
+	Sprite[] sprites;
+
+	string spritePath = "Sprites/Scenario/Lab/";
+	string spriteName = "elevator-1x2";
+
 	void Start () {
 		nextFloor = currentFloor;
+
+		sr = GetComponent <SpriteRenderer> ();
+		sprites = Resources.LoadAll<Sprite>(spritePath + spriteName);
+
+		//stopSprite = Resources.Load("elevator-1x2_0", typeof(Sprite)) as Sprite;
+		//upSprite = Resources.Load("elevator-1x2_1", typeof(Sprite)) as Sprite;
+		//downSprite = Resources.Load("elevator-1x2_2", typeof(Sprite)) as Sprite;
 	}
 
 	void Update () {
 		if (playerCheck.getIsInContact() && (currentFloor == nextFloor)) {
 			if (Input.GetKeyDown (KeyCode.UpArrow) && (floorsPosition.Length > (currentFloor + 1))) {
 				nextFloor += 1;
+				sr.sprite = sprites[1];
 			}
 			if (Input.GetKeyDown (KeyCode.DownArrow) && (currentFloor > 0)) {
 				nextFloor -= 1;
+				sr.sprite = sprites[2];
 			}
 		}
 
@@ -44,15 +65,21 @@ public class Elevator : MonoBehaviour {
 					move ();
 				} else {
 					currentFloor = nextFloor;
+					transform.position = new Vector3 (transform.position.x, floorsPosition[nextFloor], transform.position.z);
+					sr.sprite = sprites[0];
 				}
 			} else {
 				if (transform.position.y > floorsPosition[nextFloor]) {
 					move ();
 				} else {
 					currentFloor = nextFloor;
+					transform.position = new Vector3 (transform.position.x, floorsPosition[nextFloor], transform.position.z);
+					sr.sprite = sprites[0];
 				}
 			}
 		}
+
+
 	}
 
 	private void move() {
