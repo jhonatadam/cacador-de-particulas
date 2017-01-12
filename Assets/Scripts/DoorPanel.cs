bf5ff05b7c469c6db9959615d8c979cc6019f154
JoaoNSE <nobreenrique@hotmail.com>
2017-01-12 10:47:54 -0300

    Criado a porta.
    A porta foi criada (animação e script) juntamente com o painel.

---
+++ Assets/Scripts/DoorPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DoorPanel : MonoBehaviour {
+
+
+	public ContactCheck contactCheck;
+
+	private SpriteRenderer spriteRenderer;
+
+	public Door door;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+
+		spriteRenderer.color = new Vector4 (255, 0, 0, 255);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (contactCheck.getIsInContact () && Input.GetKeyDown (KeyCode.F)) {
+			door.opened = true;
+			spriteRenderer.color = new Vector4 (0, 255, 0, 255);
+		}
+
+
+
+
+
+	}
+}
