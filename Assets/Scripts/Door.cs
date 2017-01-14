1d99099e069807268d32e8a815163a7a7876c724
jhonatadam <jhonatamatias@hotmail.com>
2017-01-14 20:23:20 -0300

    Camera, Música e Cenário
    Configurada a camera da cena 2;
    Implementado luzes do cenário da cena 1;
    Inserida música nas cenas 1 e 2 (está bugada na transição de cenas).

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,22 +6,26 @@ public class Door : MonoBehaviour {
 
 	//id da porta.
 	public int id;
+
 	//Variável que indica se a porta está aberta.
 	public bool opened = false;
 
 	private Animator animator;
 
-
-
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+
+		if (opened) {
+			animator.Play ("OpenedDoor");
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (opened) {
+		if (opened && !animator.GetCurrentAnimatorStateInfo (0).IsName ("OpenedDoor")) {
 			animator.SetBool ("Opened", opened);
 		}
 	}
+
 }
---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+	public bool isBugged;
+
+	// recebe um valor de 0 à 1000
+	public int bugProbability;
+
+	private SpriteRenderer sr;
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isBugged) {
+			if (Random.Range (0, 1000) < bugProbability) {
+				if (sr.enabled) {
+					sr.enabled = false;
+				}		
+			} else {
+				if (!sr.enabled) {
+					sr.enabled = true;
+				}
+			}
+		}
+	}
+}
