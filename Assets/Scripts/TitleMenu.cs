7e081af807cd1ec785916bfd94de2f8a4c668b23
JoaoNSE <nobreenrique@hotmail.com>
2017-01-16 11:10:38 -0300

    Menu
    Adicionado o background, botões e o script. OBS: Ainda está incompleto.

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleMenu : MonoBehaviour {
+
+	public string firstSceneName;
+
+	public Button continuar;
+
+	// Use this for initialization
+	void Start () {
+		continuar.interactable = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void NovoJogo() {
+		SceneManager.LoadScene (firstSceneName);
+
+
+	}
+
+	public void Sair() {
+		Application.Quit();
+	}
+
+}
