af60089d8f72d0920777399c1a03248e5394c96d
JoaoNSE <nobreenrique@hotmail.com>
2017-01-17 21:58:14 -0200

    Modificação no prefab e script da partícula
    Foi adicionado o movimento retiníleo da partícula juntamente com a sua
    rotação.
    O gerador de partículas gera partículas com rotação aleatória.

--- Assets/Scripts/Particle.cs
+++ Assets/Scripts/Particle.cs
@@ -9,6 +9,14 @@ public class Particle : MonoBehaviour {
 
 	private float num = 0;
 
+	private float angle;
+	//transform
+	Transform trans;
+	//posição
+	Vector3 pos;
+	//rotação
+	Vector3 rot;
+
 	private Vector3 initialPosition;
 
 
@@ -16,6 +24,12 @@ public class Particle : MonoBehaviour {
 		initialPosition = transform.position;
 		TrailRenderer tr = GetComponent<TrailRenderer> ();
 		tr.sortingLayerName = TrSortingLayer;
+
+		trans = transform;
+		pos = trans.position;
+		rot = trans.rotation.eulerAngles;
+
+		Destroy (gameObject, 5f);
 	}
 	
 	void FixedUpdate () {
@@ -29,8 +43,17 @@ public class Particle : MonoBehaviour {
 	}
 
 	void LinearMovement() {
-		transform.position = initialPosition + new Vector3 (num, 0, 0);
-		//transform.position = initialPosition + transform.forward;
+		
+		//Convertendo euler's angle para radiano.
+		angle = transform.eulerAngles.magnitude * Mathf.Deg2Rad;
+
+
+		pos.x += (Mathf.Cos (angle) * step) * Time.deltaTime;
+		pos.y += (Mathf.Sin (angle) * step) * Time.deltaTime;
+	
 		num += step;
+
+		transform.position = pos;
+		transform.rotation = Quaternion.Euler(rot);
 	}
 }
--- Assets/Scripts/ParticleGenerator.cs
+++ Assets/Scripts/ParticleGenerator.cs
@@ -7,15 +7,30 @@ public class ParticleGenerator : MonoBehaviour {
 	public Transform generatePoint;
 	public GameObject particle;
 
+	public int delay;
+	private int actualTime;
+
 	// Use this for initialization
 	void Start () {
-		
+		actualTime = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//public  Quaternion rotation = Quaternion.identity;
 		//Instantiate (particle, generatePoint.position, generatePoint.rotation);
-		Instantiate (particle, generatePoint.position, new Quaternion(0, 0, 360, 0));
+
+
+		//Instantiate (particle, generatePoint.position, new Quaternion(0, 0, rnd., 0));
+		actualTime++;
+
+		if (actualTime == delay) {
+			//Instantiate (particle, generatePoint.position, new Quaternion(0, 0, Mathf.Clamp(Random.rotation.z, 300,405), 
+			//	Mathf.Clamp(Random.rotation.w, 300, 405)));
+
+			Instantiate(particle, generatePoint.position, Quaternion.Euler(0, 0, Random.Range(315, 405) + generatePoint.eulerAngles.z));
+
+			actualTime = 0;
+		}
 	}
 }
