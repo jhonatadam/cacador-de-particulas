40d84a3860c3863b346f3dece270c7e2c047feee
JoaoNSE <nobreenrique@hotmail.com>
2017-01-17 15:56:58 -0300

    TitleMenu
    Feito Tela de loading

--- Assets/Scripts/TitleMenu.cs
+++ Assets/Scripts/TitleMenu.cs
@@ -9,6 +9,7 @@ public class TitleMenu : MonoBehaviour {
 	public string firstSceneName;
 
 	public Button continuar;
+	public GameObject loadingImg;
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +22,7 @@ public class TitleMenu : MonoBehaviour {
 	}
 
 	public void NovoJogo() {
+		loadingImg.SetActive (true);
 		SceneManager.LoadScene (firstSceneName);
 
 
