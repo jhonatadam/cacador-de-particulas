6e3e4b3b5210cfe1000ce573cf2d1675215277fc
JoaoNSE <nobreenrique@hotmail.com>
2017-01-19 21:55:27 -0200

    Destruidor de Partículas
    Criado um destruidor de partículas;
    Adicionado rigidbody2D a todas as partículas.

--- Assets/ParticleDestroyer.cs
+++ Assets/ParticleDestroyer.cs
@@ -6,5 +6,11 @@ public class ParticleDestroyer : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other) {
 		Debug.Log ("ASKDJHASKJDHSKDJH");
+		Debug.Log (other.gameObject.tag);
+		if (other.gameObject.tag == "Particle") {
+			Destroy (other.gameObject);
+		}
+		//Destroy (other.gameObject);
 	}
+
 }
