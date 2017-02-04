5f6ed736e87b4fbfdfb0472c2090871174ee7bf1
JoaoNSE <nobreenrique@hotmail.com>
2017-02-04 11:18:23 -0200

    Alteração em ParticleDestroyer
    Limpeza do código.

---
+++ Assets/Scripts/ParticleDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestroyer : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Particle") {
+			Destroy (other.gameObject);
+		}
+		//Destroy (other.gameObject);
+	}
+
+}
