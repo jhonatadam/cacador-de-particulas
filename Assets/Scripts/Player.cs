1913dbb58101b2f7dedeaa47aee0ba9b5c85dc51
jhonatadam <jhonatamatias@hotmail.com>
2017-02-08 10:00:38 -0300

    refatorado animator do player
    refatorado animator do player
    isso consertou o problema da animação do player no elevador em
    movimento e possivelmente consertou o problema do player entrando no
    elevador

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,37 +11,47 @@ public class Player : MonoBehaviour {
 	public float jumpForce;
 
 	private Animator animator;
+	private SpriteRenderer sr;
+	private Rigidbody2D rb2d;
 
 	public Vector3 previousPosition;
 
 	public GroundCheck groundCheck;
-	public PlayerAnimUpdater animUpdater;
-
-	private Rigidbody2D rb2d;
 
 	private bool updateOn = true;
 
 	void Start () {
-		animator = GetComponent<Animator> ();
+		animator = GetComponent <Animator> ();
+		sr = GetComponent <SpriteRenderer> ();
 		rb2d = GetComponent<Rigidbody2D> ();
+
 		previousPosition = transform.position;
 	}
 
 	void LateUpdate() {
+		animator.SetFloat ("horizontalMovement", Mathf.Abs(rb2d.velocity.x));
+		animator.SetFloat ("verticalMovement", rb2d.velocity.y);
+
 		previousPosition = transform.position;
 	}
 
 	public void MoveHorizontally (float horizontalMovement) {
 		if (updateOn) {
-			//animator.SetFloat ("horizontalMovement", Mathf.Abs(horizontalMovement));
+			// atualizando velocidade
 			rb2d.velocity = new Vector2 (horizontalMovement * speed, rb2d.velocity.y);
-			animUpdater.UpdateAnim (gameObject, horizontalMovement);
+
+			// atualizando animator
+			UpdateSpriteDirection (horizontalMovement);
 		}	
 	} 
 
 	public void Jump () {
 		if (updateOn && groundCheck.isGrounded ()) {
+			// atualizando velocidade
 			rb2d.AddForce (new Vector2 (0, jumpForce));
+
+			// atualizando animator
+			animator.SetBool("jump", true);
 		}
 	}
 
@@ -66,4 +76,11 @@ public class Player : MonoBehaviour {
 		return updateOn;
 	}
 
+	private void UpdateSpriteDirection (float horizontalMovement) {
+		if (horizontalMovement < 0.0f) {
+			sr.flipX = true; 
+		} else if (horizontalMovement > 0.0f) {
+			sr.flipX = false;
+		} 
+	}
 }
