e993d89fceb31143b95d1b3f42ab61ef1ea01489
jhonatadam <jhonatamatias@hotmail.com>
2017-03-29 00:45:51 -0300

    criado objeto tempdata

---
+++ Assets/TempData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempData : MonoBehaviour {
+
+	public DoorState[] doorState;
+
+	public int[] elevatorFloors;
+
+	void Update () {
+		
+	}
+}
