32a3371b250cd85100bc5580fe75ef5028b0d5dc
jhonatadam <jhonatamatias@hotmail.com>
2017-04-24 20:09:50 -0300

    Evento 1
    Implementadas as componentes provisórias do evento 1

---
+++ Assets/Scripts/InteractionScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionScreen : MonoBehaviour {
+
+	public SpriteRenderer arrow;
+	public GameObject screen;
+
+	// Use this for initialization
+	void Start () {
+		arrow.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			arrow.enabled = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			arrow.enabled = false;
+		}
+	}
+
+	void showScreen() {
+
+	}
+}
