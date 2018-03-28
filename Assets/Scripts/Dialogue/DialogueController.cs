a20a79e8f2badbd2180babe05c829c102a353ccc
richallyson <richallyson@gmail.com>
2018-03-28 02:55:48 -0300

    Tentando corrigir essa bagaça

---
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour {
+	public float textFrequency = 0.1f;
+	public string[] dialogues;
+	public int currentDialogue = 0;
+	bool finished = false;
+	bool animating = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void textAnimation(){
+
+	}
+
+	void nextDialogue(){
+		if (currentDialogue < dialogues.Length - 1) {
+			currentDialogue++;
+		} else {
+			finished = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Dialogue/DialogueText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueText {
+
+	public string autor;
+	public string[] texto;
+
+}
