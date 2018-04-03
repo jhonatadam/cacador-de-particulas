75790bffc51f36665286e5084a2070f47298fa86
richallyson <richallyson@gmail.com>
2018-04-02 23:50:13 -0300

    Adicionando itens de cenário
    Melhorando a identidade visual do level design do jogo

---
+++ Assets/Scripts/Shake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour {
+	private Vector3 originalpos;
+	public float phase;
+	public float frequency;
+	public float amplitude;
+	// Use this for initialization
+	void Start () {
+		originalpos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float osc = Mathf.Sin ((Time.time +phase) * frequency)*amplitude;
+		transform.position = new Vector3 (originalpos.x + osc, originalpos.y + osc / 2.0f, originalpos.z);
+	}
+}
---
+++ Assets/Scripts/Tickle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tickle : MonoBehaviour {
+	private SpriteRenderer sr;
+	private Color originalColor;
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+		originalColor = sr.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float osc = (Mathf.Sin (Time.time) + 1) / 1.0f;
+		float osc2 = (Mathf.Sin (Time.time*1-8) + 1) / 1.0f;
+		print (osc);
+		sr.color = new Color (originalColor.r, originalColor.g * osc, originalColor.b * osc2, osc2);
+	}
+}
