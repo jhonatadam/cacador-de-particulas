79024dab18b6435787d44b8d5d716f1a801d27f7
richallyson <richallyson@gmail.com>
2018-04-03 16:21:07 -0300

    Level 1.7 Finalizado
    Graças a deus

--- Assets/Scripts/Tickle.cs
+++ Assets/Scripts/Tickle.cs
@@ -14,7 +14,7 @@ public class Tickle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float osc = (Mathf.Sin (Time.time) + 1) / 1.0f;
-		float osc2 = (Mathf.Sin (Time.time*1-8) + 1) / 1.0f;
+		float osc2 = (Mathf.Sin (Time.time*1) + 1) / 1.0f;
 		print (osc);
 		sr.color = new Color (originalColor.r, originalColor.g * osc, originalColor.b * osc2, osc2);
 	}
