a44b7cbcc018a16c99c2633bb4df2f3c65bc2dba
JoaoNSE <nobreenrique@hotmail.com>
2018-04-09 02:13:54 -0300

    Animação de início da batalha com zango
    Animação de início da batalha com zango e ajustes menores

---
+++ Assets/Scripts/Enemy/Zango/ZangoBattleStarter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZangoBattleStarter : MonoBehaviour {
+
+	public GameObject camera1;
+	public GameObject camera2;
+	public GameObject zango;
+	public GameObject escotilha;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			escotilha.SetActive (true);
+			camera1.SetActive (false);
+			camera2.SetActive (true);
+			camera2.GetComponent<Animator> ().Play ("CameraFadeOut");
+			zango.SetActive (true);
+			Destroy(gameObject.GetComponent<Collider2D>());
+		}
+	}
+}
