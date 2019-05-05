4f58c523a69fb1be3d9607753fb763307c3dfea0
richallyson <richallyson@gmail.com>
2019-05-05 16:35:21 -0300

    Pause Menu
    Adicionado menu pause provisório

---
+++ Assets/NewPauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewPauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }else{
+                Paused();
+            }
+        }
+
+    }
+
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Paused(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("NewMenuTitle");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
