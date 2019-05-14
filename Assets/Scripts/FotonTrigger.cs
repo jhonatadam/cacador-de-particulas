8dff2b8f76da40a9930ef7b6aa068bd6002c5496
richallyson <richallyson@gmail.com>
2019-05-13 21:12:39 -0300

    Adição do Foton nas fases

---
+++ Assets/Scripts/FotonTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FotonTrigger : MonoBehaviour {
+	private FotonPosition ft;
+	public GameObject foton;
+	// Use this for initialization
+	void Start () {
+		ft = GetComponentInChildren<FotonPosition>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			GameObject fot = Instantiate (foton);
+			fot.transform.position = ft.transform.position;
+			Destroy (gameObject);
+		}
+	}
+}
